e9767b5fddb6576d5c1d280b05ced7ae1d969f7c
Baisalya <baishalya1999@gmail.com>
2023-02-20 15:51:48 +0530

    http req res added to signup method

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:homerentpro/constraints/global_variables.dart';
 
+import '../constraints/error_handling.dart';
+import '../constraints/utill.dart';
 import '../model/user.dart';
 import 'package:http/http.dart'as http;
 class AuthService{
   void signUpUser(
   {
+    required BuildContext  context,
   required String email,
   required String password,
   required String name,
@@ -18,7 +23,20 @@ class AuthService{
      isAdmin: true,
      username:'',
    );
-   http.post(Uri.parse('$uri/api/auth/register'));
+   http.Response res=await http.post(
+     Uri.parse('$uri/api/auth/register'),
+     body:user.toJson(),
+     headers: <String,String>{
+       'Content-Type':'application/json; charset=UTF-8'
+     }
+   );
+   print(res.statusCode);
+   httpErrorHandel(
+       response:res,
+       context:context,
+       onSuccess: (){
+         showSnackBar(context,'Account Created!Login with credintial');
+       });
   } catch(e){}
 
   }
